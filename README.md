aa8eb6b8e96e30a8b304acf24995bc67deff3c1a
yucho147 <yucho147@gmail.com>
2022-01-08 21:05:24 +0900

    [FIX] コードのパスを変更

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Bayesian_Survival_Analysis
+## 環境設定
+### Julia
+```
+] activate .
+```
+### Python
+```
+```
+## 疑似データ作成
+```
+julia --project=@. generate_data.jl
+```
--- generate_data.jl
+++ generate_data.jl
@@ -7,8 +7,9 @@ data = YAML.load_file("test.yaml"; dicttype=Dict{Symbol,Any})
 Random.seed!(data[:seed])
 
 df = DataFrame(
-  old = cat([src.generateold(;data[:generateold][i]...) for i in 1:3]..., dims=1),
-  gender = cat([src.generategender(;data[:generategender][i]...) for i in 1:3]..., dims=1),
+  old = cat([Bayesian_Survival_Analysis.generateold(;data[:generateold][i]...) for i in 1:3]..., dims=1),
+  gender = cat([Bayesian_Survival_Analysis.generategender(;data[:generategender][i]...) for i in 1:3]..., dims=1),
   workertype = cat(ones(Int, data[:proper_num]), ones(Int, data[:mid_career_num]) * 2, ones(Int, data[:high_career_num]) * 3, dims=1)
 )
-insertcols!(df, 1, :gakui => src.generategakui(;df=df, stats=data[:generategakui][:stats]))
+insertcols!(df, 1, :gakui => Bayesian_Survival_Analysis.generategakui(;df=df, stats=data[:generategakui][:stats]))
+show(df)
