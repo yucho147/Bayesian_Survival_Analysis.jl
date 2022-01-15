f6d026134d7b2dfc5a700ed986aed84fa54c3450
yucho147 <yucho147@gmail.com>
2022-01-15 20:41:16 +0900

    [REFACTOR] コメントを追加

--- generate_data.jl
+++ generate_data.jl
@@ -7,12 +7,17 @@ data = YAML.load_file("test.yaml"; dicttype=Dict{Symbol,Any})
 Random.seed!(data[:seed])
 
 df = DataFrame(
+  # 入社した時の年齢
   old = cat([Bayesian_Survival_Analysis.generateold(;data[:generateold][i]...) for i in 1:3]..., dims=1),
+  # 性別
   gender = cat([Bayesian_Survival_Analysis.generategender(;data[:generategender][i]...) for i in 1:3]..., dims=1),
+  # 役職(1: 新卒, 2: 中途, 3: 役員)
   workertype = cat(ones(Int, data[:proper_num]), ones(Int, data[:mid_career_num]) * 2, ones(Int, data[:high_career_num]) * 3, dims=1)
 )
+# 学位(0: 高卒, 1: 学士, 2: 修士, 3: 博士)
 insertcols!(df, 1, :gakui => Bayesian_Survival_Analysis.generategakui(;df=df, stats=data[:generategakui][:stats]))
 
+# 入社年(何年前に入社したか?)
 insertcols!(df, 1, :yearofjoin => Bayesian_Survival_Analysis.generateyearofjoin(;df=df, stats=data[:generateyearofjoin][:stats]))
 
 show(df)
